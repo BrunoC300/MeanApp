3723f7382cb3e9fb4e364894656f19c4e4259ffb
BrunoC300 <brunocunha57@gmail.com>
2021-07-01 16:19:39 +0100

    added: File upload

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -24,6 +25,7 @@ mongoose
 });*/
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/images", express.static(path.join("backend/images")));
 
 //Cors setup
 app.use((req, res, next) => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
@@ -25,7 +25,7 @@ import { AppRoutingModule } from './app-routing.module';
     PostListComponent
   ],
   imports: [
-    BrowserModule, FormsModule, BrowserAnimationsModule, MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule, MatExpansionModule, HttpClientModule, AppRoutingModule, MatProgressSpinnerModule
+    BrowserModule, ReactiveFormsModule, BrowserAnimationsModule, MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule, MatExpansionModule, HttpClientModule, AppRoutingModule, MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
